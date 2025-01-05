9df4407759a1942fa0582e5bb1748337c4dd0b8d
Apram Ahuja <36915004+apramm@users.noreply.github.com>
2025-01-05 10:34:04 -0800

    Time: 6 ms (83.44%), Space: 31.4 MB (40.66%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,37 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        num_set = set()
+        # O(n) time and O(n) space
+        # *unordered* SET with more space usage but better time
+
+        #idea : no duplicates in set
+        # for num in nums:
+        #     num_set.add(num)
+        # if len(nums) == len(num_set):
+        #     return False
+        # return True
+          
+        #same idea but faster as no len checks  
+        for num in nums:
+            if num in num_set:
+                return True
+            else:
+                num_set.add(num)
+        return False
+
+        # SORTING APPROACH
+        # O(nlogn) time O(1) space Balanced approach
+        # nums.sort()
+        # for idx in range(1,len(nums)):
+        #     if nums[idx] == nums[idx-1]:
+        #         return True
+        # return False
+
+        #BASE APPROACH
+        # O(n^2) time O(1) space 
+        # for i in range(0,len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         if nums[i]==nums[j]:
+        #             return True
+                   
+        # return False            
