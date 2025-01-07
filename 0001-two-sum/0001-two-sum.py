e1b2f22f974d1c3c8c8afb7d594edae538c0a1f9
Apram Ahuja <36915004+apramm@users.noreply.github.com>
2025-01-07 00:48:06 -0800

    Time: 0 ms (100%), Space: 18.8 MB (18.69%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,7 +1,31 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        #basic soln idea
-        #do double for loop to add and check if they are equal to target & return
+        #trival len(nums) == 2
+        if len(nums) == 2:
+            return [0,1] 
+
+        #better soln ( OPTIMAL ) HASH MAP
+        # time : O(n) space: O(n)
+        # important thing to understand is to create hashmap on the go 
+
+        nums_hash = {}
+        diff = 0
+
+        for idx in range(len(nums)):
+            diff = target - nums[idx]
+            if diff in nums_hash:
+                return [idx, nums_hash[diff]]
+            else:
+                nums_hash[nums[idx]] = idx
+        return
+    
+           
+           
+
+            
+
+        #basic soln idea (NOT OPTIMAL)
+        #Double for loop to sum and check if they are equal to target & return
 
         #time : O(n^2) space : O(1)
         for i in range(len(nums)):
