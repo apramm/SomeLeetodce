ce84a40dae55babb27daf88328d43228f8e84d22
Apram Ahuja <36915004+apramm@users.noreply.github.com>
2025-01-07 00:49:27 -0800

    Update 0217-contains-duplicate.py

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,12 +4,12 @@ class Solution:
         # O(n) time and O(n) space
         # *unordered* SET with more space usage but better time
 
-        #idea : no duplicates in set
-        # for num in nums:
-        #     num_set.add(num)
-        # if len(nums) == len(num_set):
-        #     return False
-        # return True
+        #IDEA : no duplicates in set
+        for num in nums:
+            num_set.add(num)
+        if len(nums) == len(num_set):
+            return False
+        return True
           
         #same idea but faster as no len checks  
         for num in nums:
@@ -21,17 +21,18 @@ class Solution:
 
         # SORTING APPROACH
         # O(nlogn) time O(1) space Balanced approach
-        # nums.sort()
-        # for idx in range(1,len(nums)):
-        #     if nums[idx] == nums[idx-1]:
-        #         return True
-        # return False
+        
+        nums.sort()
+        for idx in range(1,len(nums)):
+            if nums[idx] == nums[idx-1]:
+                return True
+        return False
 
         #BASE APPROACH
         # O(n^2) time O(1) space 
-        # for i in range(0,len(nums)):
-        #     for j in range(i+1,len(nums)):
-        #         if nums[i]==nums[j]:
-        #             return True
+        for i in range(0,len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]==nums[j]:
+                    return True
                    
-        # return False            
+        return False            
