d3dc11734f3ecb5d6b7e51a2728f1d3a791e8316
Apram Ahuja <36915004+apramm@users.noreply.github.com>
2025-01-08 00:11:54 -0800

    Time: 11 ms (82.4%), Space: 21.4 MB (37.41%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -5,29 +5,32 @@ class Solution:
         #find anagrams within the list of strs by iterating and store
         # IMPORTANT HINT : 
         # in python list cannot be keys as immutable so store as tuples
+        # also defaultdict to avoid append and long key error
 
         #TRIVIAL length = 1
         length = len(strs)
         if length == 1:
             return [strs]
         
-        #to find anagrams
-        # do i need to see each char or no? hmm 
         # i feel that strings can also be sorted as per ascii but that will be too poor time complexity around n^2logn
-        # res = []
-        # sorted_map = {}
-        # for idx in range(length):
-        #     sort = sorted(strs[idx])
-        #     if sort in sorted_map:
-        #         sorted_map[sort].add(strs[idx])
-        #     sorted_map[sort] = strs[idx]
         
-        # print(sorted_map)
+        sort_map = defaultdict(list)
+
+        for s in strs:
+
+            sort = tuple(sorted(s))
+            
+            sort_map[sort].append(s)
+        
+        return(list(sort_map.values()))
             
 
         #trying soln without sorting using HASHMAP smart key method
-        res_list = []
+        # hard to come up with the key storing method but idea is intuitive
+        #TIME : O(n*m) SPACE : O(1) or O(m) 
+        
         res_map = defaultdict(list)
+
         for s in strs: #each str in strs
             key = [0]*26 #specific char pattern for key
 
