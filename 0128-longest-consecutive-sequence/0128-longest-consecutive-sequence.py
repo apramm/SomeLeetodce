658c3e7b5bcd0d9a5012b5e21b07775646be6b2f
Apram Ahuja <36915004+apramm@users.noreply.github.com>
2025-06-12 00:41:10 -0700

    Time: 205 ms (5.02%), Space: 40.8 MB (7.57%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,19 +1,14 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        
-        mp=defaultdict(int) #hashmap with int:key and int:value
-        result=0
+        mp = defaultdict(int)
+        result = 0
 
         for num in nums:
-            # if num not in the map already then start counting sequence 
-            if(not mp[num]):
-                #dp kindof functions
-                #count the length of sequence in a dynamic programming manner
-                mp[num] = mp[num-1]+mp[num+1]+1 
-                #extend the value of length to the boundaries
+            if not mp[num]:
+                mp[num] = mp[num-1]+mp[num+1]+1
+                #update ends
                 mp[num-mp[num-1]] = mp[num]
                 mp[num+mp[num+1]] = mp[num]
 
-                result = max(result, mp[num])
-
+                result = max(result,mp[num])
         return result
