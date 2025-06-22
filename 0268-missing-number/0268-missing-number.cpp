1993a71390a16d97e85fee9b9ab25069970d85b5
Apram Ahuja <36915004+apramm@users.noreply.github.com>
2025-06-22 03:55:27 -0700

    Time: 0 ms (100%), Space: 21.8 MB (70.15%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n = nums.size()+1; // the ideal range
-        for (int num = 0; num <= n; num++){
-            auto found = std::find(nums.begin(), nums.end(), num);
-            if(found != nums.end()){
-                continue;
-            }else{
-                return num;
-            }
+        // OPTIMAL
+        int res = nums.size(); //initialize result with n
+        for (int i = 0; i < nums.size(); i++){
+            res += i - nums[i];
         }
-        return NULL; 
+        return res;
+
+
+        // NOT GOOD AS O(N2 COMPLEXITY DUE TO FIND)
+        // int n = nums.size()+1; // the ideal range
+        // for (int num = 0; num <= n; num++){
+        //     auto found = std::find(nums.begin(), nums.end(), num);
+        //     if(found != nums.end()){
+        //         continue;
+        //     }else{
+        //         return num;
+        //     }
+        // }
+        // return NULL; 
     }
 };
