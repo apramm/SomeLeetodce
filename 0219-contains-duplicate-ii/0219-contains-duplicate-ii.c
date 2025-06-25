6596b23911b0157b485bdbb8ec991e3d652363ac
Apram Ahuja <36915004+apramm@users.noreply.github.com>
2025-06-24 17:23:19 -0700

    Time: 32 ms (81.32%), Space: 31.5 MB (32.53%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.c
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.c
@@ -1,30 +1,3 @@
-// bool containsNearbyDuplicate(int* nums, int numsSize, int k) {
-//     // TODO: find if there is an duplicate k distance from i (i != j)
-//     for (int i = 0; i < numsSize; i++){ // go through arr and check till kth distance variable if it's equal and update the k val
-//     int temp = k;
-//         while(temp!=0){
-//             if(i+temp >= numsSize){
-//                 temp -=1;
-//             }else if(nums[i] != nums[i+temp]){ 
-//                 // if the numbers not match check all the before ones
-//                 temp -= 1;
-//             }else if(nums[i] == nums[i+temp]){
-//                 return true;
-//             }
-//         }
-//     }
-//     return false;
-
-//     //OPTIMIZED Using HASHMAP maybe? in Cpp
-//     // make a seen map/set {val:index}
-//     // iterate over list and try to find the value in map O(1) lookup
-//     // compare if the difference of indices <= k if yes, return true
-//     // else insert in the map
-//     // after iteration return false
-
-    
-// }
-
 struct HashNode {
     int key;
     int value;
@@ -32,16 +5,43 @@ struct HashNode {
 };
 
 bool containsNearbyDuplicate(int* nums, int numsSize, int k) {
-    struct HashNode** hashMap = (struct HashNode**)calloc(numsSize, sizeof(struct HashNode*));
 
-    for (int i = 0; i < numsSize; ++i) {
-        int currentNum = nums[i];
-        int hashIndex = abs(currentNum) % numsSize;
+    // TODO: find if there is an duplicate k distance from i (i != j)
+    // MY SOLUTION IN FIRST 
+    // for (int i = 0; i < numsSize; i++){ // go through arr and check till kth distance variable if it's equal and update the k val
+    // int temp = k;
+    //     while(temp!=0){
+    //         if(i+temp >= numsSize){
+    //             temp -=1;
+    //         }else if(nums[i] != nums[i+temp]){ 
+    //             // if the numbers not match check all the before ones
+    //             temp -= 1;
+    //         }else if(nums[i] == nums[i+temp]){
+    //             return true;
+    //         }
+    //     }
+    // }
+    // return false;
 
-        struct HashNode* node = hashMap[hashIndex];
-        while (node) {
-            if (node->key == currentNum) {
-                if (i - node->value <= k) {
+    //OPTIMIZED Using HASHMAP IDEATION
+    // make a seen map/set {val:index}
+    // iterate over list and try to find the value in map O(1) lookup
+    // compare if the difference of indices <= k if yes, return true
+    // else insert in the map
+    // after iteration return false
+
+
+    struct HashNode** hashMap = (struct HashNode**)calloc(numsSize, sizeof(struct HashNode*)); //allocate hashmap which is a pointer to a pointer to hashnode and initialize to val to 0
+
+    for (int i = 0; i < numsSize; ++i) { //for each index
+        int currentNum = nums[i]; // current num
+        int hashIndex = abs(currentNum) % numsSize; // create a hashIndex
+
+        struct HashNode* node = hashMap[hashIndex]; // create a pointer to the hashnode positioned at hashIndex
+
+        while (node) { //
+            if (node->key == currentNum) { // if the node->key match num
+                if (i - node->value <= k) { // if index - node->values <= k
                     free(hashMap);
                     return true;
                 } else {
