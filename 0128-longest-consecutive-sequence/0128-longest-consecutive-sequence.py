5699541897cdb35c55362865b08dc51d053ad02c
Apram Ahuja <36915004+apramm@users.noreply.github.com>
2025-07-18 09:13:29 -0700

    Time: 43 ms (90.03%), Space: 34.3 MB (31.18%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -4,15 +4,10 @@ class Solution:
         longest = 0
 
         for num in nums_set:
-            # reach the min value in nums_set and then do stuff
             if num-1 not in nums_set:
-                current = num
-                streak = 1
-
-                while current+1 in nums_set:
-                    current+=1
-                    streak+=1
-
-                longest = max(longest,streak)
+                length = 1
+                while num+length in nums_set:
+                    length +=1
+                longest = max(longest, length)
 
         return longest
