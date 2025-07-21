f270dcd27a25c1ef4ffdb552eec6b3da57168bda
Apram Ahuja <36915004+apramm@users.noreply.github.com>
2025-07-20 23:36:56 -0700

    Time: 0 ms (100%), Space: 19.1 MB (10.29%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,43 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        #BASE CASE len(nums) == 2
-        if len(nums) == 2:
-            return [0,1] 
-
-        # OPTIMAL IDEATION
-        # lets try the set implementation for optimal runtime
-
-        # some issue that arise: 
-        # how to not run into the issue of returning the same number in set
-        # how to be able to return the index if both same number?
-
-        # to solve both these issues we use hashmap
-        # where our runtime of lookup isn't compromised but
-        # we create {[num:index],[num:index]} kind of hashmap
+        # kuch check krke dena arr mei toh map/set try
+        complement_map = {} #value->index
         
+        for i,num in enumerate(nums):
+            if target - num in complement_map:
+                return [complement_map[target-num], i]
+            complement_map[num] = i
 
-        #better soln ( OPTIMAL ) HASH MAP
-        # time : O(n) space: O(n)
-        # important thing to understand is to create hashmap on the go 
+        return []
 
-        nums_hash = {}
-        diff = 0
-
-        for idx in range(len(nums)):
-            diff = target - nums[idx]
-            if diff in nums_hash:
-                return [idx, nums_hash[diff]]
-            else:
-                nums_hash[nums[idx]] = idx
-        return
-    
-
-        #basic soln idea (NOT OPTIMAL)
-        #Double for loop to sum and check if they are equal to target & return
-
-        #time : O(n^2) space : O(1)
-        # for i in range(len(nums)):
-        #     for j in range(i+1,len(nums)):
-        #         if nums[i]+nums[j] == target:
-        #             return [i,j]
-
+        
