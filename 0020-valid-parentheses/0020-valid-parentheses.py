e0651c5832fdc7276588d3a10d7b0e75d7107402
Apram Ahuja <36915004+apramm@users.noreply.github.com>
2025-07-22 00:12:03 -0700

    Time: 0 ms (100%), Space: 17.7 MB (80.81%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -7,11 +7,15 @@ class Solution:
         mapping = {")":"(", "}":"{","]":"["}
 
         for c in s:
-            if c in mapping.values():
+            if c in mapping:
+                # for closing bracket, check stack
+                if stack and stack[-1] == mapping[c]:
+                    stack.pop() #remove matching opening bracket
+                else:
+                    return False #if latest bracket isn't matching
+            else:
+                #push opening bracket on stack
                 stack.append(c)
-            elif c in mapping.keys():
-                if not stack or mapping[c] != stack.pop():
-                    return False
 
-        return not stack
+        return not stack #true if stack empty
 
