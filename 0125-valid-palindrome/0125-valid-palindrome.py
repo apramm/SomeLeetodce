6ca30d9786965f51d8102acddfe4190e18eb3b67
Apram Ahuja <36915004+apramm@users.noreply.github.com>
2025-07-23 00:02:27 -0700

    Time: 3 ms (99%), Space: 19.6 MB (20.53%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -2,9 +2,28 @@ import re
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         # just do check for normal string with reversed string?
+        # REGEX + REVERSE METHOD
+
         s = s.lower() #upper to lower
-        s = re.sub(r'[^a-zA-Z0-9]', '', s) #regex to include only valid alphanumeric values i.e., a-zA-Z0-9
+        s = re.sub(r'[^a-zA-Z0-9]', '', s) 
+        #regex to include only valid alphanumeric values i.e., a-zA-Z0-9
+        #regex, replace non accepting with empty string, s
         return s == s[::-1]
+
+
+        #sliding window
+        left = 0
+        right = len(s)-1
+        
+        while left<right:
+            if s[left] != s[right]:
+                return False
+            left +=1
+            right -=1
+
+        return True
+
+
 
 
         
