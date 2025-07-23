f7fa46787c4b07752f0345d2bc627d4efe613363
Apram Ahuja <36915004+apramm@users.noreply.github.com>
2025-07-22 23:49:17 -0700

    Time: 70 ms (41.32%), Space: 18 MB (83.54%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -1,15 +1,28 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        string_map = {} #string->freq
+        #hashmap method
+        # string_map = {} #string->freq
+
+        # for c in s:
+        #     if c in string_map:
+        #         string_map[c] += 1
+        #     else:
+        #         string_map[c] = 1
+
+        # for i,c in enumerate(s):
+        #     if string_map[c] == 1:
+        #         return i
+            
+        # return -1
+
+        #ascii method
+
+        freq = [0]*26
 
         for c in s:
-            if c in string_map:
-                string_map[c] += 1
-            else:
-                string_map[c] = 1
-
+            freq[ord(c) - ord("a")] += 1
+        
         for i,c in enumerate(s):
-            if string_map[c] == 1:
+            if freq[ord(c) - ord("a")] == 1:
                 return i
-            
         return -1
