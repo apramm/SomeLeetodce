e1b6fd6181cf8882ed6aa16197057b611a272241
Apram Ahuja <36915004+apramm@users.noreply.github.com>
2025-07-24 00:20:01 -0700

    Time: 47 ms (66.48%), Space: 20.3 MB (11.73%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -6,10 +6,11 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
+        #my soln O(n) time and space
         # idea to have a set which keeps on updating as we iterate thru linkedlist
         # if the element alr in the set then we've cycle else we dont
         # spinned around on value wayy to long bro just store the whole thing :/
-        
+
         if head == None : return False
 
         setNodes = set()
@@ -23,4 +24,20 @@ class Solution:
             head = head.next
             
         return False
+
+        # follow up, can it be done in O(1) SPACE?
+        # i.e., would need to use some pointer way to know all visited nodes 
+
+        # i can update head = head.next and change the visited node's head.next as it doesn't state that i need to keep linkedlist the same way :/
+
+        curr = head
+        while head:
+                curr = head.next
+                head.next.val = -1
+                head = head.next
+                if head.val == -1:
+                    return True
+
+        return False
+
         
