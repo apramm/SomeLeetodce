f0e3e03f9975540b847b485e29714547055cae47
Apram Ahuja <36915004+apramm@users.noreply.github.com>
2025-11-13 21:56:11 -0800

    Time: 0 ms (100%), Space: 19.1 MB (10.45%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,13 +1,31 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # kuch check krke dena arr mei toh map/set try
-        complement_map = {} #value->index
-        
-        for i,num in enumerate(nums):
-            if target - num in complement_map:
-                return [complement_map[target-num], i]
-            complement_map[num] = i
+        # todo : return indices of two numbers in nums so,  == target 
+        # 0-based indexing
 
+        # assumption allowed : 
+        # each input has exactly one solution
+        # cannot use the same element twice
+
+        #more questions?
+        # are numbers non-negative?
+        
+
+        # idea : use hashmap to store number itself as key and value as it's index
+        # put all numbers and their indices in map 
+        # subtract and done
+         
+        map = {} # {num:i, ...}
+        
+        # loop over nums
+        # if complement in map : return map[complement], i
+        # edge: what if same number then; we add stuff only if we dont find complement
+        # that way if the same number came we've it's index and initial number's index
+
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in map:
+                return [map[complement], i]
+            map[num] = i
+        
         return []
-
-        
