cbd3edaa7a897a05c7f87e0a6746ded5c70df702
Apram Ahuja <36915004+apramm@users.noreply.github.com>
2026-02-17 22:50:36 -0800

    0112-PATH-SUM Stats: Time: 0 ms (100%), Space: 20.1 MB (67.03%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -6,18 +6,36 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        if not root: # edge case: empty tree
-            return False
-        
-        targetSum -= root.val # subtract current node's value from targetSum
+        # recursive approach
 
-        if not root.left and not root.right: # check if it's a leaf node
-            return targetSum == 0 # if targetSum is 0, we found a valid path
+        # if not root: # edge case: empty tree
+        #     return False
         
-        # recursively check left and right subtrees
-        if self.hasPathSum(root.left, targetSum):
-            return True
-        if self.hasPathSum(root.right, targetSum):
-            return True
+        # targetSum -= root.val # subtract current node's value from targetSum
+
+        # if not root.left and not root.right: # check if it's a leaf node
+        #     return targetSum == 0 # if targetSum is 0, we found a valid path
         
-        return False # if neither subtree has a valid path, return False
+        # # recursively check left and right subtrees
+        # if self.hasPathSum(root.left, targetSum):
+        #     return True
+        # if self.hasPathSum(root.right, targetSum):
+        #     return True
+        
+        # return False # if neither subtree has a valid path, return False
+    
+        #iterative approach
+
+        if not root:  
+            return False
+        stack = [(root, targetSum - root.val)] # stack to hold nodes and remaining sum
+        while stack:
+            node, current_sum = stack.pop()
+            if not node.left and not node.right and current_sum == 0: #check if it's a leaf node and current_sum is 0
+                return True
+            if node.right:
+                stack.append((node.right, current_sum - node.right.val))
+            if node.left:
+                stack.append((node.left, current_sum - node.left.val))
+        return False
+    
