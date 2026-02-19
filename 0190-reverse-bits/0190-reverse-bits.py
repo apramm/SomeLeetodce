5f48bfdec7f7f7432e1e82ffa1ee352fe6cd90d5
Apram Ahuja <36915004+apramm@users.noreply.github.com>
2026-02-19 00:55:28 -0800

    0190-REVERSE-BITS Stats: Time: 48 ms (41.8%), Space: 19.4 MB (10.79%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -2,7 +2,8 @@ class Solution:
     def reverseBits(self, n: int) -> int:
         result = 0
         for i in range(32):
-            result = (result << 1) | (n & 1)
-            n >>= 1
+            result = (result << 1) | (n & 1) 
+            # shift result left and add the least significant bit of n
+            n >>= 1 # shift n right to process the next bit
         return result
         
